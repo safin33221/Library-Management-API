9c01dff2f7bebc4db8de8b6d123e4ce81f39f3dd
safin33221 <safin33221@gmail.com>
2025-06-20 20:01:20 +0600

    Connected with database.

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,17 @@
+import express, { Request, Response } from 'express'
+import { Application } from "express";
+import cors from 'cors';
+
+
+const app: Application = express()
+app.use(cors());
+
+app.use(express.json())
+
+app.get('/', (req: Request, res: Response) => {
+    res.send(`Library management server is running`)
+})
+
+export default app
+
 
---
+++ src/server.ts
@@ -0,0 +1,25 @@
+import { Server } from "http";
+import mongoose from "mongoose";
+import app from "./app";
+
+require('dotenv').config()
+const PORT = 8000;
+let server: Server;
+const uri = process.env.MONGODB_URI
+async function main() {
+    try {
+        if (!uri) {
+            throw new Error('mongodb env variable not found')
+        }
+        await mongoose.connect(uri);
+        console.log("db is connected");
+        server = app.listen(PORT, () => {
+            console.log(`app is running or port ${PORT}`);
+        })
+
+    } catch (error) {
+        console.log('sever error', error); 
+    }
+}
+
+main()
